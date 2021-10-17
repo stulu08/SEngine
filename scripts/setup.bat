1b8e3dc08487475f029ede1f697ebfea8af76061
Stulu <derstulu@gmail.com>
2021-10-17 17:40:32 +0200

    setup.bat is now installing python correctly

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -1,6 +1,20 @@
 @echo off
-echo trying to install python from Microsoft Store
+python --version 3>NUL
+if errorlevel 1 goto errorNoPython
+
+echo installing requests
 python3 -m pip install requests
+echo Trying installing vulkan
 setupVulkan.py
+echo Trying to generate Project Files for Visual Studio 2019
 generateProjects.bat
+pause
+close
+
+goto:eof
+
+:errorNoPython
+echo Trying to install python from Microsoft Store
+python
+echo After installing python execute this file again
 pause
